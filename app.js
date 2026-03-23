95a578a954ed92d03af7441a1126e813887f4414
Shahzeb Zafar <zabilala99@gmail.com>
2026-03-23 05:10:31 +0500

    checking the CI for CI Pipeline

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello DevOps World");
+  res.end("CI is added to the projectv - v2");
 });
 
 server.listen(3000, () => {
